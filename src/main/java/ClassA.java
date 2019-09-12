ab3ecd8891ef3c2feb343aabbec4a980b1ff62a3
asami <vbo.secondary@gmail.com>
2019-09-12 13:52:08 +0300

    commit

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -2,3 +2,15 @@ public class ClassA {
     //checkout
     //pyschpysch
 }
+    IntelliJ IDEA 2019.3 Snapshot (Ultimate Edition)
+        Build #IU-193.3334, built on September 12, 2019
+        IntelliJ IDEA EAP User
+        Expiration date: October 12, 2019
+        Runtime version: 11.0.4+12-b462.3 amd64
+        VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o
+        Linux 5.0.0-23-generic
+        GC: ParNew, ConcurrentMarkSweep
+        Memory: 1231M
+        Cores: 2
+        Registry:
+        Non-Bundled Plugins:
